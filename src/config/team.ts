679da4412d9e663a4b7b58c93dde558036e45203
JoelFarias <joelcarolinofarias@gmail.com>
2026-09-12 15:40:10 -0300

    fix: melhora enquadramento das fotos da equipe

--- src/config/team.ts
+++ src/config/team.ts
@@ -11,7 +11,7 @@ export const portalTeamMembers: readonly TeamMember[] = [
     id: 'mateus-de-lima-costa', name: 'Mateus de Lima Costa', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/mateus-lima-costa.webp', linkedin: 'https://www.linkedin.com/in/mateus-de-lima-costa', lattes: 'https://lattes.cnpq.br/8852621038958616', github: 'https://github.com/MateusDeLimaCosta',
   },
   {
-    id: 'pedro-henrique-de-oliveira-marques', name: 'Pedro Henrique de Oliveira Marques', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/pedro-marques.webp', linkedin: 'https://www.linkedin.com/in/pedro-henrique-de-oliveira-marques-76253a324/', lattes: 'https://lattes.cnpq.br/8932115738920035', github: 'https://github.com/PedroOliveiraMarques',
+    id: 'pedro-henrique-de-oliveira-marques', name: 'Pedro Henrique de Oliveira Marques', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/pedro-marques-v2.jpg', linkedin: 'https://www.linkedin.com/in/pedro-henrique-de-oliveira-marques-76253a324/', lattes: 'https://lattes.cnpq.br/8932115738920035', github: 'https://github.com/PedroOliveiraMarques',
   },
   {
     id: 'jose-roberto-steiner-de-moura', name: 'José Roberto Steiner de Moura', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', linkedin: 'https://www.linkedin.com/in/roberto-steiner', lattes: 'http://lattes.cnpq.br/0843344698115019', github: 'https://github.com/jrsmoura',
@@ -32,7 +32,7 @@ export const portalTeamMembers: readonly TeamMember[] = [
     id: 'diego-alexandre-rodrigues-de-sa', name: 'Diego Alexandre Rodrigues de Sá', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', linkedin: 'https://www.linkedin.com/in/diegoalexandresa/', lattes: 'https://lattes.cnpq.br/5685899832458406', github: 'https://github.com/diegolamarck',
   },
   {
-    id: 'felipe-melo-moreira-nunes', name: 'Felipe Melo Moreira Nunes', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/felipe-moreira.webp', linkedin: 'https://www.linkedin.com/in/felipemoreiradt', lattes: 'https://lattes.cnpq.br/1844341937323514', github: 'https://github.com/felipemoreira-data',
+    id: 'felipe-melo-moreira-nunes', name: 'Felipe Melo Moreira Nunes', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/felipe-moreira-v2.jpg', linkedin: 'https://www.linkedin.com/in/felipemoreiradt', lattes: 'https://lattes.cnpq.br/1844341937323514', github: 'https://github.com/felipemoreira-data',
   },
   {
     id: 'samuel-tavares-rego', name: 'Samuel Tavares Rego', role: 'Integrante do Projeto Big Data IESB', category: 'Equipe do Projeto', photoUrl: '/img/team/samuel-tavares.webp', linkedin: 'https://www.linkedin.com/in/samuel-tavares-1518b4278/', lattes: 'http://lattes.cnpq.br/7850764502442411', github: 'https://github.com/SamuelTARE',
